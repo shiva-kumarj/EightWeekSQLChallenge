33cc08a0e2c7bf4f61793e8b227f4b398cee42b2
shiva-kumarj <electrodude1995@gmail.com>
2023-01-20 13:57:47 -0500

    incomplete file

--- pizza_runner_CS2/ingredient_optimization.sql
+++ pizza_runner_CS2/ingredient_optimization.sql
@@ -1,5 +1,30 @@
 -- C. Ingredient Optimisation
 -- 1. What are the standard ingredients for each pizza?
+select * from pizza_toppings;
+
+with split_recipes(pizza_id, ingredient, toppings) as 
+(
+    select pizza_id, 
+            left(toppings, charindex(',', toppings + ',') - 1),
+            stuff(toppings, 1, charindex(',', toppings + ','), '')
+            from pizza_recipes
+
+    union all
+
+    select pizza_id, 
+            left(toppings, charindex(',', toppings + ',') - 1),
+            stuff(toppings, 1, charindex(',', toppings + ','), '')
+            from split_recipes
+    where toppings > ''
+)
+select pizza_id, ingredient into unique_recipes
+from split_recipes
+order by pizza_id;
+
+select * from unique_recipes;
+
+select pizza_id, cast(ingredient as int) as ing, ingredient 
+from unique_recipes;
 -- 2. What was the most commonly added extra?
 -- 3. What was the most common exclusion?
 -- 4. .Generate an order item for each record in the customers_orders table in the format of one of the following:
