ad1ee6b76621e0f4a6f0c7242327dce15765c7e2
shiva-kumarj <electrodude1995@gmail.com>
2023-03-17 20:25:52 -0400

    Pizza runner case study COMPLETE

--- pizza_runner_CS2/price_and_ratings/question1.sql
+++ pizza_runner_CS2/price_and_ratings/question1.sql
@@ -104,3 +104,19 @@ from
     on pn.pizza_id = co.pizza_id
     where distance != '0'
 ) as sub;
+
+-- E. Bonus Questions
+-- If Danny wants to expand his range of pizzas - how would this 
+-- impact the existing data design? Write an INSERT statement 
+-- to demonstrate what would happen if a new Supreme pizza with 
+-- all the toppings was added to the Pizza Runner menu?
+
+-- Adding supreme pizza to pizza_recipes and pizza_names tables
+insert into pizza_recipes(pizza_id, toppings)
+values (3, '1,2,3,4,5,6,7,8,9,10,11,12');
+insert into pizza_names(pizza_id, pizza_name)
+values (3, 'Supreme');
+
+-- Additionally some customers might order these pizzas to
+-- corresponding entries in the customer_order and runner_orders 
+-- table must be made.
